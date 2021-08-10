eb3fca47000978969b09d5f8c0f94024c08a68d0
Gabriel Morandi de Mello <61758357+gabrielmorandi@users.noreply.github.com>
2021-08-09 21:15:48 -0300

    Add files via upload

--- agenda final.py
+++ agenda final.py
@@ -2,8 +2,8 @@
 #Gustavo Paixão Machado - 202040602016
 
 #criação das listas e dos dicionários e variaveis
-agenda = [{'nome': 'Gustavo Paixao', 'email': 'gustavop@gmail.com', 'telefone': 949999999, 'sexo': 'm', 'idade': 19}, {'nome': 'Gustavo Paixao', 'email': 'gustavoc@gmail.com', 'telefone': 9488888888, 'sexo': 'f', 'idade': 9}]
-dados_cadastro = {'nome': 'Gustavo Corvo', 'email': 'gustavoc@gmail.com', 'telefone': 9488888888, 'sexo': 'f', 'idade': 9}
+agenda = [{'nome': 'Gustavo Paixao', 'email': 'gustavop@gmail.com', 'telefone': 949999999, 'sexo': 'm', 'idade': 19}, {'nome': 'Gabriel', 'email': 'gariel@gmail.com', 'telefone': 94977777777, 'sexo': 'm', 'idade': 19}, {'nome': 'Gustavo Paixao', 'email': 'gustavoc@gmail.com', 'telefone': 9488888888, 'sexo': 'f', 'idade': 9}]
+dados_cadastro = {}
 busca = []
 pesquisa = []
 a = 0
@@ -13,6 +13,7 @@ telefone = 0
 sexo = ''
 idade = 0
 oque = ''
+deleta = []
 # Criacao da funcao cadastro de contato
 def cadastro_contato(nome, email, telefone, sexo, idade):
     # Cadastrar um novo contato
@@ -35,14 +36,19 @@ def consulta_contato(nome_contato):
         # informar se foi ou nao encontrado
     else:
         print('Contato encontrado!\n', pesquisa)
+# criando a funcao para alterar o contato 
 def altera_contato(nome, oque):
+    # pesquisa o contato que quer alterar
     pesquisa = [p for p in agenda if p['nome'] == nome]
-    print(len(pesquisa))
+        # se nao achar nenhum contato retornar contato nao encontrado
     if len(pesquisa) == 0:
         print('Contato nao encontrado!')
+        # se encontrar um contato ele mesmo sera alterado
     elif len(pesquisa) == 1:
+        print('Contato: ', pesquisa)
         pesquisa[0][oque] = input(f'Informe o(a) novo(a) {oque} do contato: ')
         print('Contato alterado com sucesso, confira abaixo como ficou o contato!\n', pesquisa)
+        # se encontrar mais de um contato igual selecione qual vai ser alterado
     elif len(pesquisa) > 1:
         print(pesquisa)
         n = int(input('Informe qual desse contatos acima voce deseja alterar (ex: para o primeiro contato digite 1, para o segundo contato digite 2): '))
@@ -55,6 +61,46 @@ def altera_contato(nome, oque):
         else:
             pesquisa = [p for p in agenda if p['nome'] == alteracao]
             print('Contato alterado com sucesso, confira abaixo como ficou o contato!\n', pesquisa)
+# criando a funcao para excluir um contato
+def excluir_contato(nome):
+    pesquisa = [p for p in agenda if p['nome'] == nome]
+    # se nao encontrar nenhnum contato informado 
+    if len(pesquisa) == 0:
+        print('Nenhum contato encontrado!')
+    # se encontrar um contato informado
+    elif len(pesquisa) == 1:
+        print(pesquisa)
+        quer = input('Deseja excluir o contato (s ou n): ')
+        if quer == 's':
+            i = 0
+            while i != len(agenda):
+                if agenda[i]['nome'] == nome:
+                    deleta.append(i)
+                i = i + 1
+            if len(deleta) == 1:
+                del(agenda[deleta[0]])
+                print('Contato excluído com sucesso!')
+        else:
+            print('Contato NAO excluido!')
+    # se encontrar mais de um contato informado
+    elif len(pesquisa) > 1:
+        print(pesquisa)
+        n = int(input('Informe qual desse contatos acima voce deseja alterar (ex: para o primeiro contato digite 1, para o segundo contato digite 2): '))
+        n = n - 1
+        print(pesquisa[n])
+        quer = input('Deseja excluir o contato (s ou n): ')
+        if quer == 's':
+            i = 0
+            while i != len(agenda):
+                if agenda[i]['nome'] == nome:
+                    deleta.append(i)
+                i = i + 1
+            if len(deleta) > 1:
+                del(agenda[deleta[n]])
+                print('Contato excluído com sucesso!')
+        else:
+            print('Contato NAO excluido!')
+# menu
 while a == 0:
     print('========================================================')
     print('1. Cadastrar um novo contato;')
@@ -87,4 +133,6 @@ while a == 0:
         while oque != 'nome' and oque != 'email' and oque != 'telefone' and oque != 'sexo' and oque != 'idade':
             oque = input('Informe oque deseja alterar (nome, email, telefone, sexo, idade): ')
         altera_contato(nome_contato, oque)
-            
+    elif escolha == 4:
+        nome_contato = input('Informe o nome do contato que deseja excluir: ')
+        excluir_contato(nome_contato)
